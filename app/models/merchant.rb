e4d28581b8e9598283414ae0b8463bfc1b1e597f
Scott Borecki <scottborecki@gmail.com>
2021-09-05 14:26:37 -0600

    Add .order_by_name

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,10 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :items
 
   validates :name, presence: true
+
+  def self.order_by_name(order = 'asc')
+    order = 'asc' unless order == 'desc'
+
+    order(name: order)
+  end
 end
