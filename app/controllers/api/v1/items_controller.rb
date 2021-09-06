cdad749060dab94f19eff48638d8b8483a8168d4
Scott Borecki <scottborecki@gmail.com>
2021-09-06 15:58:28 -0600

    Create Pageable module for pagination

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,10 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
-  # TODO: Refactor action by abstracting page, per_page, and offset to module?
-  #       Will likely be able to reuse for other controllers/actions
+  include Pageable
+
   def index
-    page = params.fetch(:page, 1).to_i
-    per_page = params.fetch(:per_page, 20).to_i
-    offset = page <= 1 ? 0 : (page - 1) * per_page
     items = Item.all.offset(offset).limit(per_page)
     formatted_items = ItemSerializer.format_items(items)
     json_response(formatted_items)
