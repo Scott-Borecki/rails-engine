97b1e7344c026642555b4d110b2abb5b3cd02b4b
Scott Borecki <scottborecki@gmail.com>
2021-09-06 10:39:11 -0600

    Add merchant_with_revenue factory

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -3,3 +3,14 @@ FactoryBot.define do
     name { Faker::Company.name }
   end
 end
+
+def merchant_with_revenue(invoice_items_count: 4)
+  FactoryBot.create(:merchant) do |merchant|
+    FactoryBot.create(:item, merchant: merchant) do |item|
+      FactoryBot.create(:invoice, merchant: merchant, status: 'shipped') do |invoice|
+        FactoryBot.create(:transaction, result: 'success', invoice: invoice)
+        FactoryBot.create_list(:invoice_item, invoice_items_count, item: item, invoice: invoice)
+      end
+    end
+  end
+end
