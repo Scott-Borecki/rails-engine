97746b39d5870838bec0eae4f7d851257025b002
Scott Borecki <scottborecki@gmail.com>
2021-09-06 10:32:25 -0600

    Add Merchant-Invoice relationship

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,5 @@
 class Merchant < ApplicationRecord
+  has_many :invoices, dependent: :destroy
   has_many :items, dependent: :destroy
   has_many :invoice_items, through: :items
 
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   describe 'relationships' do
+    it { should have_many(:invoices).dependent(:destroy) }
     it { should have_many(:items).dependent(:destroy) }
     it { should have_many(:invoice_items).through(:items) }
   end
