bfb39453c874d3bd3b53cef4cbcd4cb91d0769e7
Scott Borecki <scottborecki@gmail.com>
2021-09-06 19:48:41 -0600

    Create factories for with and without revenue

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -5,3 +5,21 @@ FactoryBot.define do
     merchant
   end
 end
+
+def invoice_with_revenue(ii_count: 4, status: 'shipped', result: 'success')
+  FactoryBot.create(:invoice, status: status) do |invoice|
+    FactoryBot.create(:item) do |item|
+      FactoryBot.create(:transaction, result: result, invoice: invoice)
+      FactoryBot.create_list(:invoice_item_fixed, ii_count, item: item, invoice: invoice)
+    end
+  end
+end
+
+def invoice_without_revenue(ii_count: 4, status: 'shipped', result: 'failed')
+  FactoryBot.create(:invoice, status: status) do |invoice|
+    FactoryBot.create(:item) do |item|
+      FactoryBot.create(:transaction, result: result, invoice: invoice)
+      FactoryBot.create_list(:invoice_item_fixed, ii_count, item: item, invoice: invoice)
+    end
+  end
+end
