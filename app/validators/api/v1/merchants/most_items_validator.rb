fc7588563e3d729b10952df8e5b267742fe7f178
Scott Borecki <scottborecki@gmail.com>
2021-09-08 10:35:39 -0600

    Add Merchants::MostItemsValidator

---
+++ app/validators/api/v1/merchants/most_items_validator.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    module Merchants
+      class MostItemsValidator
+        include ActiveModel::Validations
+
+        attr_accessor :quantity
+
+        validates :quantity, presence: true, numericality: { allow_nil: true, greater_than: 0 }
+
+        def initialize(data = {})
+          @quantity = data[:quantity]
+        end
+      end
+    end
+  end
+end
---
+++ spec/validators/api/v1/merchants/most_items_validator_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+describe Api::V1::Merchants::MostItemsValidator, type: :validator do
+  describe 'object creation' do
+    describe 'valid objects' do
+      context 'when I provide a quantity string' do
+        it 'creates a valid object' do
+          find_params = { quantity: '10' }
+          validator = Api::V1::Merchants::MostItemsValidator.new(find_params)
+
+          expect(validator).to be_valid
+        end
+      end
+
+      context 'when I provide an quantity integer' do
+        it 'creates a valid object' do
+          find_params = { quantity: 10 }
+          validator = Api::V1::Merchants::MostItemsValidator.new(find_params)
+
+          expect(validator).to be_valid
+        end
+      end
+
+    end
+
+    describe 'invalid objects' do
+      context 'when I do not provide a quantity' do
+        it 'returns an invalid object' do
+          validator = Api::V1::Merchants::MostItemsValidator.new
+          error_message = {:quantity=>["can't be blank"]}
+
+          expect(validator).not_to be_valid
+          expect(validator.errors.messages).to eq(error_message)
+        end
+      end
+
+      context 'when I provide a quantity as a letter string' do
+        it 'returns an invalid object' do
+          find_params = { quantity: 'some-quantity' }
+          validator = Api::V1::Merchants::MostItemsValidator.new(find_params)
+          error_message = {:quantity=>["is not a number"]}
+
+          expect(validator).not_to be_valid
+          expect(validator.errors.messages).to eq(error_message)
+        end
+      end
+
+      context 'when I provide a quantity as a number/letter string' do
+        it 'returns an invalid object' do
+
+        find_params = { quantity: 'some-quantity-5' }
+        validator = Api::V1::Merchants::MostItemsValidator.new(find_params)
+        error_message = {:quantity=>["is not a number"]}
+
+        expect(validator).not_to be_valid
+        expect(validator.errors.messages).to eq(error_message)
+        end
+      end
+    end
+  end
+end
