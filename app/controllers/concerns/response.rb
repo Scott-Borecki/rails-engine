6eaffac6d7e2db2d3c6c7e07790633adfa4600e0
Scott Borecki <scottborecki@gmail.com>
2021-09-08 08:47:16 -0600

    Add #json_error_response

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -2,4 +2,8 @@ module Response
   def json_response(object, status = :ok)
     render json: object, status: status
   end
+
+  def json_error_response(object, status = :bad_request)
+    render json: { error: object, status: status.to_s.gsub('_', ' ').titleize, code: Rack::Utils::SYMBOL_TO_STATUS_CODE[status] }, status: status
+  end
 end
